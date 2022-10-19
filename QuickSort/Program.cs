22a4641d7b689f41e2ee767398725c3396b477cb
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2022-10-19 17:41:07 +0700

    Add method display

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -103,6 +103,19 @@ namespace QuickSort
             // sort the list on the right of pivot using quick sort
             q_sort(j + 1, high);
         }
+        void display()
+        {
+            Console.WriteLine("\n---------------------");
+            Console.WriteLine(" Sorted array elements ");
+            Console.WriteLine("-----------------------");
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\n Number of comparisons : " + cmp_count);
+            Console.WriteLine("\n Number of data movements : " + mov_count);
+        }
         static void Main(string[] args)
         {
         }
