2f213262f405d12b94287d27d955d574ed07024a
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2022-10-19 16:49:01 +0700

    Add array of integers

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace QuickSort
 {
-    internal class Program
+    class Program
     {
+        // array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of sata movement
+
+        // number of elements in array
+        private int n;
         static void Main(string[] args)
         {
         }
